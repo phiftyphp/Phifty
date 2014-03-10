47c9cd710bf6067eeec7e38adb2566eecf3beaee
c9s <yoanlin93@gmail.com>
2014-03-10 22:04:14 +0800

    Add FacebookService

--- src/Phifty/Service/FacebookService.php
+++ src/Phifty/Service/FacebookService.php
@@ -6,11 +6,20 @@ use ConfigKit\Accessor;
 use Universal\Container\ObjectContainer;
 use Exception;
 
+/*
+  FacebookService:
+    appId: 327281450625323
+    secret: 721be7a340cc0b4b5073071cabb8d26b
+*/
+
 class FacebookService implements ServiceInterface
 {
     public function getId() { return 'Facebook'; }
 
     public function register($kernel, $options = array() )
     {
+        $kernel->facebook = function() use ($options) {
+            return new Facebook($options);
+        };
     }
 }
