6dce3105ef5edf39b690e2c3ca48c45a20140b24
c9s <yoanlin93@gmail.com>
2014-03-13 17:46:58 +0800

    clean up

--- src/Phifty/Service/AesCryptorService.php
+++ src/Phifty/Service/AesCryptorService.php
@@ -22,34 +22,25 @@ class AesCryptor {
     */
     public function encrypt( $msg, $base64 = false ) {
         $k = $this->key;
- 
         # open cipher module (do not change cipher/mode)
-        if ( ! $td = mcrypt_module_open('rijndael-128', '', 'ctr', '') )
+        if ( ! $td = mcrypt_module_open('rijndael-128', '', 'ctr', '') ) {
             return false;
- 
-        // $msg = serialize($msg);                         # serialize
+        }
 
         $iv = mcrypt_create_iv(16, MCRYPT_RAND);        # create iv
- 
         if ( mcrypt_generic_init($td, $k, $iv) !== 0 )  # initialize buffers
             return false;
 
-        echo base64_encode($iv) . "\n";
- 
         $msg = mcrypt_generic($td, $msg);               # encrypt
-
-        echo base64_encode($msg) . "\n";
-
         $msg = $iv . $msg;                              # prepend iv
 
         $mac = $this->pbkdf2($msg, $k, 1000, 16);       # create mac
         $msg .= $mac;                                   # append mac
- 
         mcrypt_generic_deinit($td);                     # clear buffers
         mcrypt_module_close($td);                       # close cipher module
- 
-        if ( $base64 ) $msg = base64_encode($msg);      # base64 encode?
- 
+        if ( $base64 ) {
+            $msg = base64_encode($msg);      # base64 encode?
+        }
         return $msg;                                    # return iv+ciphertext+mac
     }
  
@@ -64,33 +55,32 @@ class AesCryptor {
     */
     public function decrypt( $msg, $base64 = false ) {
         $k = $this->key;
- 
-        if ( $base64 ) $msg = base64_decode($msg);          # base64 decode?
- 
+        if ( $base64 ) {
+            $msg = base64_decode($msg);          # base64 decode?
+        }
+
         # open cipher module (do not change cipher/mode)
-        if ( ! $td = mcrypt_module_open('rijndael-128', '', 'ctr', '') )
+        if ( ! $td = mcrypt_module_open('rijndael-128', '', 'ctr', '') ) {
             return false;
- 
+        }
+
         $iv = substr($msg, 0, 16);                          # extract iv
         $msg = substr($msg, 16);  // remove iv
         $em = substr($msg, -16);  // extract mac
         $msg = substr($msg, 0, strlen($msg) - 16);           # extract ciphertext
         $mac = $this->pbkdf2($iv . $msg, $k, 1000, 16);     # create mac
-        if ( $em !== $mac )                                 # authenticate mac
+        if ( $em !== $mac ) {                                 # authenticate mac
             return false;
- 
-        if ( mcrypt_generic_init($td, $k, $iv) !== 0 )      # initialize buffers
+        }
+        if ( mcrypt_generic_init($td, $k, $iv) !== 0 ) {    # initialize buffers 
             return false;
- 
+        }
         $msg = mdecrypt_generic($td, $msg);                 # decrypt
-        // $msg = unserialize($msg);                           # unserialize
- 
         mcrypt_generic_deinit($td);                         # clear buffers
         mcrypt_module_close($td);                           # close cipher module
- 
         return $msg;                                        # return original msg
     }
- 
+
     /** PBKDF2 Implementation (as described in RFC 2898);
      *
      *  @param string p password
@@ -106,22 +96,17 @@ class AesCryptor {
         $hl = strlen(hash($a, null, true)); # Hash length
         $kb = ceil($kl / $hl);              # Key blocks to compute
         $dk = '';                           # Derived key
- 
         # Create key
         for ( $block = 1; $block <= $kb; $block ++ ) {
- 
             # Initial hash for this block
             $ib = $b = hash_hmac($a, $s . pack('N', $block), $p, true);
- 
             # Perform block iterations
-            for ( $i = 1; $i < $c; $i ++ )
- 
+            for ( $i = 1; $i < $c; $i ++ ) {
                 # XOR each iterate
                 $ib ^= ($b = hash_hmac($a, $b, $p, true));
- 
+            }
             $dk .= $ib; # Append iterated block
         }
- 
         # Return derived key of correct length
         return substr($dk, 0, $kl);
     }
