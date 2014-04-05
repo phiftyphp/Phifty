43e4f6c0c16445fc18d8d17fd6b573f3d8682c7d
c9s <yoanlin93@gmail.com>
2014-04-05 22:53:45 +0800

    Add BuildCOmmand

---
+++ src/Phifty/Command/BuildCommand.php
@@ -0,0 +1,22 @@
+<?php
+namespace Phifty\Command;
+use CLIFramework\Command;
+use Roller\Dumper\ConsoleDumper;
+
+class BuildCommand extends Command
+{
+    public function brief() {
+        return 'Build component files.';
+    }
+
+    public function init()
+    {
+        $this->registerCommand('router', 'Phifty\Command\BuildRouterCommand');
+    }
+
+    public function execute()
+    {
+        $this->logger->info('Available commands: router');
+    }
+
+}
