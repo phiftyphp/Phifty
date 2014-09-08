8b0e4f8602e9927846063268223dbc11f031e256
c9s <yoanlin93@gmail.com>
2014-09-08 18:45:25 +0800

    Move main.php to main_app.php and update installer

--- main.php
+++ main.php
@@ -2,4 +2,4 @@
 define('PH_ROOT', __DIR__);
 define('PH_APP_ROOT',__DIR__);
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor/corneltek/phifty-core/src/Phifty/Bootstrap.php';
+require __DIR__ . '/src/Phifty/Bootstrap.php';
