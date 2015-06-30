1a0732c440537a81ec6d3f983e9d9206ac2d8c25
c9s <yoanlin93@gmail.com>
2015-06-30 23:57:05 +0800

    Remove outdated tests

--- tests/Phifty/ExcelTest.php
+++ tests/Phifty/ExcelTest.php
@@ -5,6 +5,9 @@ class ExcelTest extends PHPUnit_Framework_TestCase
 {
     function testExcel()
     {
+        if (!class_exists('PHPExcel')) {
+            $this->markTestSkipped('PHPExcel is required for testing');
+        }
         $excel = new ExcelExporter;
         $excel->setSheet(0);
         $excel->putCells( array('Test','Foo','Bar') );
