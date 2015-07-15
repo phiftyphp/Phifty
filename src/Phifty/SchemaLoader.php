14d9ab224cb243eb7c59071b2c612619923b773d
c9s <yoanlin93@gmail.com>
2015-07-15 17:20:27 +0800

    Find schema files safely

--- src/Phifty/SchemaLoader.php
+++ src/Phifty/SchemaLoader.php
@@ -1,14 +1,19 @@
 <?php
 $finder = new LazyRecord\Schema\SchemaFinder;
-foreach ( kernel()->applications as $app ) {
-    $finder->in( $app->locate() );
+if ($apps = kernel()->applications) {
+    foreach ($apps as $app) {
+        $finder->in( $app->locate() );
+    }
 }
 
-foreach ( kernel()->bundles as $bundle ) {
-    $finder->in( $bundle->locate() );
+
+if ($bundles = kernel()->bundles) {
+    foreach ($bundles as $bundle) {
+        $finder->in( $bundle->locate() );
+    }
 }
 
-if ( file_exists('tests') ) {
+if (file_exists('tests')) {
     $finder->in('tests');
 }
 $finder->find();
