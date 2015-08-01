7fe192808029479ad2271d20538808b4220c6962
c9s <yoanlin93@gmail.com>
2015-08-01 13:48:36 +0800

    Just install apcu extension

--- scripts/install-apcu.sh
+++ scripts/install-apcu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [[ ${TRAVIS_PHP_VERSION:0:3} != "5.4" ]] ; then
-    echo "no" | pecl install apcu-4.0.7
-fi
+# if [[ ${TRAVIS_PHP_VERSION:0:3} != "5.4" ]] ; then
+echo "no" | pecl install apcu-4.0.7
+# fi
