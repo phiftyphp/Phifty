e11460e1030c0358c7b4ca8f71459e1098edd441
c9s <yoanlin93@gmail.com>
2015-08-23 19:47:14 +0800

    Rename RouterServiceTest

--- tests/Phifty/ServiceProvider/RollerRouterServiceTest.php
+++ tests/Phifty/ServiceProvider/RollerRouterServiceTest.php
@@ -1,11 +1,11 @@
 <?php
 
-class RouterServiceTest extends PHPUnit_Framework_TestCase
+class RollerRouterServiceTest extends PHPUnit_Framework_TestCase
 {
     public function testRouterService()
     {
         $kernel = new Phifty\Kernel;
-        $service = new Phifty\ServiceProvider\RouterServiceProvider;
+        $service = new Phifty\ServiceProvider\RollerRouterServiceProvider;
         $kernel->registerService($service);
         ok($kernel->router);
         ok($kernel->router instanceof Roller\Router );
