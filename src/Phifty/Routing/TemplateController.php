804359781751e7fb3b18e3774ab7e3a8b6a146af
c9s <yoanlin93@gmail.com>
2015-08-29 23:20:52 +0800

    reconcile TemplateController's constructor with 2.0 Pux router

--- src/Phifty/Routing/TemplateController.php
+++ src/Phifty/Routing/TemplateController.php
@@ -1,17 +1,22 @@
 <?php
 namespace Phifty\Routing;
 use Phifty\View\Engine;
-use Roller\Controller;
+use Pux\Controller\Controller;
 
 class TemplateController extends Controller
 {
     public $template;
-    public $args;
 
-    public function __construct($args)
+    public $args = array();
+
+    public function __construct(array $environment, array $matchedRoute)
     {
+        parent::__construct($environment, $matchedRoute);
+        list($pcre, $path, $callback, $options ) = $matchedRoute;
+
+        $args = $options['args'];
         $this->template = $args['template'];
-        $this->args = isset($args['template_args']) ? $args['template_args'] : null;
+        $this->args = isset($args['template_args']) ? $args['template_args'] : array();
     }
 
     public function run()
