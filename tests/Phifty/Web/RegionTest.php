aa453d38065a8694916d18031e374cf25dfc465d
c9s <yoanlin93@gmail.com>
2015-08-29 15:34:17 +0800

    Remove xhprof listener

--- tests/Phifty/Web/RegionTest.php
+++ tests/Phifty/Web/RegionTest.php
@@ -1,15 +1,29 @@
 <?php
+use Phifty\Web\Region;
 
 class RegionTest extends PHPUnit_Framework_TestCase
 {
-    function test()
+    public function testGetRegionId()
     {
-        $region = new Phifty\Web\Region('/bs/news/crud/edit',array( 
+        $region = new Region('/bs/news/crud/edit',array( 
             'id' => 1
         ));
         $id = $region->getRegionId();
         ok($id);
         ok($region->render());
+    }
+
+    public function testRegionWithPath()
+    {
+        $region = new Region('/bs/product/crud/create');
+        $html = $region->render();
+        $this->assertNotNull($html);
+    }
+
+    public function testRegionIdFactory()
+    {
+        $id = Region::newRegionSerialId();
+        $this->assertTrue(is_string($id));
     }
 }
 
