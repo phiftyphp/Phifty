18e7a42cbea8fa4e9489bddbd898449d9845fe90
c9s <yoanlin93@gmail.com>
2015-08-29 15:59:55 +0800

    Clean up roller code from PuxRouterServiceProvider

--- src/Phifty/ServiceProvider/PuxRouterServiceProvider.php
+++ src/Phifty/ServiceProvider/PuxRouterServiceProvider.php
@@ -16,29 +16,6 @@ class PuxRouterServiceProvider implements ServiceProvider
         $kernel->router = function() use ($kernel) {
             return new Mux;
         };
-        /*
-        $kernel->restful = function() use($kernel) {
-            $restful = new \Roller\Plugin\RESTful(array( 
-                'prefix' => '/restful'
-            ));
-            return $restful;
-        };
-        $kernel->router = function() use ($kernel) {
-            $router = null;
-            if ('production' === $kernel->environment) {
-                $router = new Router(null, array(
-                    'route_class' => 'Phifty\\Routing\\Route',
-                    'cache_id' => PH_APP_ROOT ? PH_APP_ROOT : $kernel->config->get('framework','uuid'),
-                ));
-            } else {
-                $router = new Router(null, array(
-                    'route_class' => 'Phifty\\Routing\\Route',
-                ));
-            }
-            $router->addPlugin($kernel->restful);
-            return $router;
-        };
-        */
     }
 }
 
