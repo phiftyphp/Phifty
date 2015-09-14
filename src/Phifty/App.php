f18461240697e41b4d77184ee59e054dc2ff06e2
c9s <yoanlin93@gmail.com>
2015-09-14 15:09:07 +0800

    Add Phifty\App with Funk\App interface

---
+++ src/Phifty/App.php
@@ -0,0 +1,30 @@
+<?php
+namespace Phifty;
+use Funk\Compositor;
+
+class App implements \Funk\App
+{
+
+    public function call(array $environment, array $response)
+    {
+        return $response;
+    }
+
+
+    /**
+     * The default PHPSGI application builder (logics for wrapping application with middlewares)
+     *
+     * @return callable
+     */
+    static public function build()
+    {
+        return new static;
+        /*
+        $compositor = new Compositor(new static);
+        return $compositor->wrap();
+         */
+    }
+}
+
+
+
