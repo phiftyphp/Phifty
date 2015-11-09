79aaddb5c629a535ee876d099e43cc18e432ee5f
c9s <yoanlin93@gmail.com>
2015-11-09 20:27:26 +0800

    Update KendoServiceProvider for Kendo 2.0

--- src/Phifty/ServiceProvider/KendoServiceProvider.php
+++ src/Phifty/ServiceProvider/KendoServiceProvider.php
@@ -1,27 +1,71 @@
 <?php
 namespace Phifty\ServiceProvider;
-use Kendo\Acl\RuleLoader;
-use Kendo\Acl\Acl;
+use Kendo\SecurityPolicy\SecurityPolicyModule;
+use Kendo\RuleLoader\RuleLoader;
+use Kendo\RuleLoader\SchemaRuleLoader;
+use Kendo\RuleMatcher\AccessRuleMatcher;
+use Kendo\Authorizer\Authorizer;
+use Kendo\IdentifierProvider\ActorIdentifierProvider;
+use Kendo\Operation\GeneralOperation;
+use LazyRecord\ConnectionManager;
+
+class KendoService
+{
+    protected $options;
+
+    protected $ruleLoader;
+
+    public function __construct(array $options)
+    {
+        $this->options = $options;
+        if (isset($options['SecurityPolicies'])) {
+            $policies = $options['SecurityPolicies'];
+        }
+        if (!isset($options['RuleLoader']['PDORuleLoader'])) {
+            throw new Exception('Missing RuleLoader settings');
+        }
+
+    }
+
+    public function getRuleLoader()
+    {
+        if ($this->ruleLoader) {
+            return $this->ruleLoader;
+        }
+        $dataSource = $this->options['RuleLoader']['PDORuleLoader']['DataSource'];
+        $connectionManager = ConnectionManager::getInstance();
+        $conn = $connectionManager->getConnection($dataSource);
+        $this->ruleLoader = new PDORuleLoader;
+        $this->ruleLoader->load($conn);
+        return $this->ruleLoader;
+    }
+
+    public function getAuthorizer()
+    {
+        if ($this->authorizer) {
+            return $this->authorizer;
+        }
+        $authorizer = new Authorizer;
+        $accessRuleMatcher = new AccessRuleMatcher($this->getRuleLoader());
+        $authorizer->addMatcher($accessRuleMatcher);
+        return $authorizer;
+    }
+}
 
 class KendoServiceProvider extends BaseServiceProvider
 {
+    public function getId() { return 'access_control'; }
 
-    /**
-     * The RuleLoader
-     */
-    public $loader;
-
-    public function getId() { return 'KendoAccessControl'; }
-
-    public function register($kernel,$options = array())
+    public function register($kernel, $options = array())
     {
         $self = $this;
-        $kernel->acl = function() use ($self,$kernel,$options) {
-            $loader = new RuleLoader;
-            foreach ($options['Rules'] as $ruleClass) {
-                $loader->load($ruleClass);
-            }
-            return Acl::getInstance($loader);
+        $kernel->accessControl = function() use ($self, $kernel, $options) {
+            return new KendoService($options);
+            /*
+            $actor = new NormalUser;
+            $ret = $authorizer->authorize($actor, GeneralOperation::VIEW, 'products');
+            var_dump($ret);
+            */
         };
     }
 }
