03920640da61bd0048da3e28161336c80390e721
c9s <yoanlin93@gmail.com>
2016-04-24 02:31:54 +0800

    Don't scan all bundle folders

--- Phifty/SchemaLoader.php
+++ Phifty/SchemaLoader.php
@@ -2,13 +2,13 @@
 use LazyRecord\Schema\SchemaLoader;
 $finder = new LazyRecord\Schema\SchemaFinder;
 if ($app = kernel()->getApp()) {
-    $finder->in($app->locate());
+    $finder->in($app->locate() . DIRECTORY_SEPARATOR . 'Model');
 }
 
 
 if ($bundles = kernel()->bundles) {
     foreach ($bundles as $bundle) {
-        $finder->in( $bundle->locate() );
+        $finder->in($bundle->locate() . DIRECTORY_SEPARATOR . 'Model');
     }
 }
 
