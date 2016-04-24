d934f92482cba8e6cab558daa37aa02898df7e98
c9s <yoanlin93@gmail.com>
2016-04-24 12:59:23 +0800

    Fix bash syntax

--- scripts/install-apcu.sh
+++ scripts/install-apcu.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pecl channel-update pecl.php.net
 if [[ ${TRAVIS_PHP_VERSION:0:1} == "5" ]] ; then
-echo "yes" | pecl install apcu-4.0.11
+    echo "yes" | pecl install apcu-4.0.11
+fi
 if [[ ${TRAVIS_PHP_VERSION:0:1} == "7" ]] ; then
-echo "yes" | pecl install apcu-5.1.3
+    echo "yes" | pecl install apcu-5.1.3
 fi
