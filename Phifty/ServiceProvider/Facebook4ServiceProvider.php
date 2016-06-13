9546e8f855aed8e07465406eb9bf843b56b6a9ef
c9s <yoanlin93@gmail.com>
2016-06-14 00:32:05 +0800

    Fix Facebook4ServiceProvider with examples

--- Phifty/ServiceProvider/Facebook4ServiceProvider.php
+++ Phifty/ServiceProvider/Facebook4ServiceProvider.php
@@ -25,22 +25,21 @@ class Facebook4ServiceProvider
 
     public function register(Kernel $kernel, $options = array())
     {
-        FacebookSession::setDefaultApplication($options['AppId'], $options['AppSecret']);
-
         $kernel->facebook = function() use ($options, $kernel) {
             $container = new Container;
+            $container['app_id'] = $options['AppId'];
             $container['session'] = function($c) use ($options) {
                 return new Facebook([
-                    'app_id'                => $options['AppId']
+                    'app_id'                => $options['AppId'],
                     'app_secret'            => $options['AppSecret'],
                     'default_graph_version' => $options['DefaultGraphVersion'],
                 ]);
             };
             // make this factory?
-            $container['redirect_login_helper'] = $c->factory(function($c) {
+            $container['redirect_login_helper'] = $container->factory(function($c) {
                 return $c['session']->getRedirectLoginHelper();
             });
-            $container['login_url'] = $c->factory(function($c) use ($options) {
+            $container['login_url'] = $container->factory(function($c) use ($options) {
                 if (preg_match('#^https?://#',$options['DefaultLoginCallbackUrl'])) {
                     $url = $options['DefaultLoginCallbackUrl'];
                 } else {
@@ -49,11 +48,14 @@ class Facebook4ServiceProvider
                 $helper = $c['redirect_login_helper'];
                 return $helper->getLoginUrl($url, $options['DefaultPermissions']);
             });
+            $container['oauth2_client'] = $container->factory(function($c) use ($options) {
+                return $c['session']->getOAuth2Client();;
+            });
             return $container;
         };
     }
 
-    public function canonicalizeConfig(Kernel $kernel, array $options)
+    static public function canonicalizeConfig(Kernel $kernel, array $options)
     {
         if (!isset($options['AppId']) || !isset($options['AppSecret'])) {
             throw new Exception('AppId or AppSecret is not defined.');
@@ -62,7 +64,7 @@ class Facebook4ServiceProvider
             $options['DefaultGraphVersion'] = 'v2.5';
         }
         if (!isset($options['DefaultPermissions'])) {
-            $options['DefaultPermissions'] = ['email'];
+            $options['DefaultPermissions'] = ['public_profile','email'];
         }
         if (!isset($options['DefaultLoginCallback'])) {
             $options['DefaultLoginCallbackUrl'] = '/oauth/facebook/callback';
