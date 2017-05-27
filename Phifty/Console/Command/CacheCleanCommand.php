ccbd8f9b9abc9927ad8e3ae306d83803b0c1044c
c9s <yoanlin93@gmail.com>
2017-05-27 23:31:48 +0900

    improve cache clean command

--- Phifty/Console/Command/CacheCleanCommand.php
+++ Phifty/Console/Command/CacheCleanCommand.php
@@ -9,19 +9,20 @@ class CacheCleanCommand extends Command
 
     public function execute() 
     {
-        if ( file_exists('cache') ) {
+        if (file_exists('cache')) {
+            $this->logger->info("Removing cache files from directory ./cache ...");
             $rit = new RecursiveIteratorIterator(new RecursiveDirectoryIterator('cache'), RecursiveIteratorIterator::CHILD_FIRST); 
             foreach ($rit as $file) { 
                 if  ( $file->isFile() ) {
                     $this->logger->info( "Removing " . $file->getPathname() );
-                    unlink( $file->getPathname() );
+                    unlink($file->getPathname());
                 }
             } 
         }
 
-        if ( extension_loaded('apc') ) {
-            apc_clear_cache();
-            apc_clear_cache('user');
+        if (extension_loaded('apcu')) {
+            $this->logger->info("Clearing apcu cache ...");
+            apcu_clear_cache();
         }
     }
 }
