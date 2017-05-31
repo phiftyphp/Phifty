a47d79f40843c31beb82729ee6bf8227e75bf774
c9s <yoanlin93@gmail.com>
2017-05-31 20:58:53 +0800

    Add general TestCase

--- Phifty/Testing/TestCase.php
+++ Phifty/Testing/TestCase.php
@@ -3,11 +3,37 @@
 namespace Phifty\Testing;
 
 use Phifty\Bootstrap;
+use ConfigKit\ConfigLoader;
+use Universal\ClassLoader\Psr4ClassLoader;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected $configLoader;
+
+    protected $kernel;
+
     public function setup()
     {
-        
+        $configLoader = new ConfigLoader;
+        $configLoader->load('framework', "config/framework.yml");
+        $configLoader->load('application', "config/application.yml");
+        $configLoader->load('testing', "config/testing.yml");
+        $this->configLoader = $configLoader;
+
+        // TODO: load Kernel from the generated app (global).
+        $this->kernel = Bootstrap::createKernel($configLoader, new Psr4ClassLoader);
     }
+
+    protected function createPostRequest($path, $parameters)
+    {
+        return [
+            'PATH_INFO' => $path,
+            'parameters' => $parameters,
+            'body_parameters' => $parameters,
+            'query_parameters' => $parameters,
+            '_SESSION' => [],
+            '_COOKIE' => [],
+        ];
+    }
+
 }
