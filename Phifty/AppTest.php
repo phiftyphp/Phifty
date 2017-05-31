062ab8d61ebeb5846f410a2baa86259a7b8b5347
c9s <yoanlin93@gmail.com>
2017-05-31 20:53:30 +0800

    Add TestCase

---
+++ Phifty/AppTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Phifty;
+
+use PHPUnit\Framework\TestCase;
+use Universal\ClassLoader\Psr4ClassLoader;
+use Funk\Environment;
+
+class AppTest extends TestCase
+{
+    public function testBuild()
+    {
+        $configLoader = Bootstrap::createConfigLoader(PH_APP_ROOT);
+        $classLoader = new Psr4ClassLoader;
+        $kernel = Bootstrap::createKernel($configLoader, $classLoader);
+        $app = App::build($kernel, []);
+        $this->assertNotNull($app);
+
+        return $app;
+    }
+
+    /**
+     * @depends testBuild
+     */
+    public function testCall($app)
+    {
+        $environment = [
+            'PATH_INFO' => '/',
+            'parameters' => [],
+            'queryParameter' => [],
+            '_SESSION' => [],
+            '_COOKIE' => [],
+        ];
+        $response = [];
+        $response = $app->call($environment, $response);
+        $this->assertNotNull($response);
+    }
+}
---
+++ Phifty/Testing/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Phifty\Testing;
+
+use Phifty\Bootstrap;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+    public function setup()
+    {
+        
+    }
+}
