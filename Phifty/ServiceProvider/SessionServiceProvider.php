70920e28553d5aa453063068c9d2bf426f4bb96f
c9s <yoanlin93@gmail.com>
2017-06-03 01:10:36 +0800

    move session initialization to boot() method

--- Phifty/ServiceProvider/SessionServiceProvider.php
+++ Phifty/ServiceProvider/SessionServiceProvider.php
@@ -19,12 +19,19 @@ class SessionServiceProvider extends BaseServiceProvider
     {
         // if we have session service provider, call the setup
         // $s = $kernel->session; // build session object and write to the buffer before we write data to the browser.
+        $kernel->session = function() {
+            return new Session([
+                "state" => new NativeState,
+                "storage" => new NativeStorage,
+            ]);
+        };
+    }
+
+    public function boot(Kernel $kernel)
+    {
         if (!$kernel->isCli) {
             // TODO: customize this for $options
-            $kernel->session = new Session([
-                'state' => new NativeState(),
-                'storage' => new NativeStorage(),
-            ]);
+            $s = $kernel->session;
         }
     }
 }
