27584ba11dd1fc8734141e1cb4f6d093893601bf
c9s <yoanlin93@gmail.com>
2017-06-03 15:19:48 +0800

    override the parent config method

--- Phifty/Testing/ModelTestCase.php
+++ Phifty/Testing/ModelTestCase.php
@@ -5,7 +5,7 @@ namespace Phifty\Testing;
 use Phifty\Bootstrap;
 use ConfigKit\ConfigLoader;
 use Universal\ClassLoader\Psr4ClassLoader;
-
+use Maghead\Runtime\Config\FileConfigLoader;
 
 abstract class ModelTestCase extends \Maghead\Testing\ModelTestCase
 {
@@ -19,6 +19,15 @@ abstract class ModelTestCase extends \Maghead\Testing\ModelTestCase
         $this->configLoader = Bootstrap::createConfigLoader(PH_APP_ROOT, $env);
         $this->kernel = Bootstrap::createKernel($this->configLoader, new Psr4ClassLoader, $env);
         parent::setup();
+    }
+
+    /**
+     * use the config/database.yml instead of the default config from maghead
+     * model test case to avoid side effect.
+     */
+    public function config()
+    {
+        return FileConfigLoader::load('config/database.yml');
     }
 
     protected function createPostRequest($path, $parameters)
