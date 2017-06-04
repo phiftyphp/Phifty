49e07449d8a9ac8fbf08f8e7d130e64f48969d32
c9s <yoanlin93@gmail.com>
2017-06-04 14:01:59 +0800

    add fixme in bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,6 +12,16 @@
 define('PH_APP_ROOT', dirname(__DIR__));
 define('PH_ROOT', dirname(__DIR__));
 define('CLI', true);
-require 'vendor/autoload.php';
 */
+
+// FIXME: always unlink the bootstrap before we test it, can we ?
+/*
+require 'vendor/autoload.php';
+if (file_exists('bootstrap.php')) {
+    unlink('bootstrap.php');
+}
+
+$app = new \Phifty\Console\Application;
+$app->run(['phifty', 'bootstrap']);
+ */
 require 'bootstrap.php';
